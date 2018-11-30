3c96e46a9026cf821d6a4bd2e1a4ffa58bb732ed
ydemangeau <ydemange@student.42.fr>
2018-11-30 11:31:42 +0100

    add fct get_str

--- src/fillit.c
+++ src/fillit.c
@@ -12,29 +12,42 @@
 
 #include "fillit.h"
 
+char		*get_str(char *line)
+{
+	char *str;
+	char *tmp;
+
+		if (!str)
+			ft_strnew(1);
+		tmp = ft_strjoin(str,line);
+		free(str);
+		str = tmp;
+	return (str);
+}
+
 t_list		*get_list(int fd)
 {
-	t_list *list;
-	char *line;
-	char *tmp;
-	char *str;
-//DECOUPER EN 2 FCT retourstr -> aloue list -> memdel str;
-	while (get_next_line(fd, &line) > 0)
+	t_list	*list;
+	char	*line;
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (get_next_line(fd,&line) > 0)
 	{
-		if (check
-		if (line[0] != '\n')
+		//chech error
+		if (i != 4)
 		{
-			if (!str)
-				ft_strnew(1);
-			tmp = ft_strjoin(str,line);
-			free(str);
-			str = tmp;
+			str = get_str(line);
+			i++;
 		}
 		else
 		{
 			if (!list)
 				list = ft_lstnew(str,ft_strlen(str));
 			ft_lstadd(&list,ft_lstnew(str,ft_strlen(str)));
+			free(str);
+			i = 0;
 		}
 		free(line);
 	}
@@ -43,14 +56,14 @@ t_list		*get_list(int fd)
 
 int		main(int argc, char **argv)
 {
-	t_list *list;
-	int fd;
-	int retour;
-	int i;
+	t_list	*list;
+	int		fd;
+	int		retour;
 
 	if (arg_error(argc,argv))
 		return (-1);
 	fd = open(argv[1],O_RDONLY);
 	list = get_list(fd);
+	//TEST LISt
 	return (0);
 }
